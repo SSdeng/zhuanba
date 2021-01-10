60ae7ae0f673cfdc361dfbb067d4ba59c7ca5448
HuaWei <wuhaizhen@mail.nwpu.edu.cn>
2021-01-10 18:28:34 +0800

    Collection实体类

--- src/main/java/com/zb/entity/Collection.java
+++ src/main/java/com/zb/entity/Collection.java
@@ -25,19 +25,26 @@ import java.util.List;
 @DynamicInsert // 动态插入，字段为空时不加入到insert语句
 @DynamicUpdate // 动态更新，字段为空时不加入到update语句
 public class Collection implements Serializable {
-
+    /**
+     * 收藏名称
+     */
     @Id
     @Column(name = "collection_name")
-    private String collectionName;//收藏名称
-
+    private String collectionName;
+    /**
+     * 所属用户，设置外键user_id，参照sys_user的id字段
+     */
     @ManyToOne(targetEntity = User.class, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinColumn(name = "user_Id", referencedColumnName = "id")
-    private User user; // 所属用户，设置外键user_id，参照sys_user的id字段
+    private User user;
+    /**
+     * 收藏列表，负责维护外键
+     */
     @ManyToMany(targetEntity = Item.class, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinTable(name = "sys_collection_item",
             // 当前对象在中间表的外键
             joinColumns = {@JoinColumn(name = "collection_name", referencedColumnName = "collection_name")},
             // 对方对象在中间表的外键
             inverseJoinColumns = {@JoinColumn(name = "item_id", referencedColumnName = "id")})
-    private List<Item> items; // 商品所有的分类标签，负责维护外键
+    private List<Item> items;
 }
