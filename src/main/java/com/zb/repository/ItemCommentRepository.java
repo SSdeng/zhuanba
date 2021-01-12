0e6cc6cc24adaf46081dc8c67812bae92e0fa571
Man_Jie <297867870@qq.com>
2021-01-13 00:35:49 +0800

    ItemComment的一些修改

--- src/main/java/com/zb/repository/ItemCommentRepository.java
+++ src/main/java/com/zb/repository/ItemCommentRepository.java
@@ -4,10 +4,22 @@ import com.zb.entity.ItemComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 /**
  * ItemComment DAO
  * shenmanjie
  * 2021/1/10 23:19
  */
 public interface ItemCommentRepository extends JpaRepository<ItemComment, Long>, JpaSpecificationExecutor<ItemComment> {
+
+
+    /**
+     * 通过ItemId返回评论该商品的所有评论
+     *
+     * @param item_id
+     * @return
+     */
+    public List<ItemComment> getByItem_IdOrderByCreateTime(Long item_id);
+
 }
