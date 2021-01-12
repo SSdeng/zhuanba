352f192aa888be613a29c4d3bbbdcc0da23cf72a
HuaWei <wuhaizhen@mail.nwpu.edu.cn>
2021-01-12 15:27:09 +0800

    Collectionxaing

--- src/main/java/com/zb/controller/CollectionController.java
+++ src/main/java/com/zb/controller/CollectionController.java
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
+/**
+ * 收藏controller
+ */
 
 @RestController
 @RequestMapping("/api/collection")
@@ -30,11 +32,24 @@ public class CollectionController {
         this.collectionService=collectionService;
     }
 
+    /**
+     * 向收藏中添加商品
+     * @param userId
+     * @param itemId
+     * @return
+     */
     @PostMapping("/add")
     public Result AddItem(@RequestParam("userId")Long  userId, @RequestParam("itemId") Long itemId){
         return Result.ok(collectionService.addItem(userId,itemId));
 
     }
+
+    /**
+     * 从收藏中移除商品
+     * @param collectionId
+     * @param itemId
+     * @return
+     */
     @PostMapping("/remove")
     public String RemoveItem(@RequestParam("collectionId")Long collectionId
             , @RequestParam("itemId") Long itemId){
