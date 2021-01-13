20e347e6cc3ca6a2d19bb2284dd9c7eb41440813
donglang <944976717@qq.com>
2021-01-13 15:26:58 +0800

    实现了商品发布功能

--- src/main/java/com/zb/controller/PageController.java
+++ src/main/java/com/zb/controller/PageController.java
@@ -6,15 +6,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PageController {
 
+//    @RequestMapping("/")
+//    public String getIndex1(){
+//        return "index";
+//    }
+
     @RequestMapping("/index")
     public String getIndex(){
         return "index";
     }
 
-    @RequestMapping("/item")
-    public String getItem(){
-        return "item";
-    }
+//    @RequestMapping("/item")
+//    public String getItem(){
+//        return "item";
+//    }
 
     @RequestMapping("/login")
     public String getLogin(){
@@ -29,5 +34,10 @@ public class PageController {
     @RequestMapping("/release")
     public String getRelease(){
         return "release";
+    }
+
+    @RequestMapping("/admin")
+    public String getAdmin(){
+        return "admin";
     }
 }
