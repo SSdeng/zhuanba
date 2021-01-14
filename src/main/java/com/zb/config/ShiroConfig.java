d4047bce5636790ec943c68ffb84e1967a49323a
dengzhijian <1433327470@qq.com>
2021-01-15 04:55:22 +0800

    shiro修改

--- src/main/java/com/zb/config/ShiroConfig.java
+++ src/main/java/com/zb/config/ShiroConfig.java
@@ -87,9 +87,7 @@ public class ShiroConfig {
         filterChainDefinitionMap.put("/api/comment/**", "roles[user]");
         filterChainDefinitionMap.put("/api/collection/**", "roles[user]");
         filterChainDefinitionMap.put("/api/admin/**", "roleOrFilter[root,admin]");
-        filterChainDefinitionMap.put("/api/admin/banAdmin", "roles[root]");
-        filterChainDefinitionMap.put("/api/admin/addAdmin", "roles[root]");
-        filterChainDefinitionMap.put("/api/admin/unbanAdmin", "roles[root]");
+        filterChainDefinitionMap.put("/api/root/**", "roles[root]");
         // 设置未授权路由，之后再返回json数据给前端
         shiroFilterFactoryBean.setLoginUrl("/login");
         // 未授权界面, 对应LoginController中 unauthorized 请求
